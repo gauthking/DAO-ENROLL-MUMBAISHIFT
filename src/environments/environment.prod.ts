81b8a1129eca65e3d609ebf97b3e76d200d76141
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2023-07-18 14:22:41 +0530

    fixed env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  MumbaiGateway: 'https://cosmopolitan-dimensional-layer.matic-testnet.discover.quiknode.pro/183cf778fe4c2260b3b7dbc9bb264848f4f3b0ee/',
+  Contract: '0xBc4f739F6310BdE8f5c48134Ca221Bb5349ccd74'
 };
