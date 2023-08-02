8422b198115b789a546427b426709fff10712046
chiranjeev13 <chiranjeevmishra13@gmail.com>
2023-08-02 20:48:42 +0530

    FE changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   MumbaiGateway: 'https://cosmopolitan-dimensional-layer.matic-testnet.discover.quiknode.pro/183cf778fe4c2260b3b7dbc9bb264848f4f3b0ee/',
-  Contract: '0xBc4f739F6310BdE8f5c48134Ca221Bb5349ccd74'
+  Contract: '0xc15a65D129D2a3D4C4Ea10270755F15E6FcF5b42'
 
 };
 
