f7d6df33040b7df93982ffdaf2440ba55ba82ad9
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2023-08-02 20:59:40 +0530

    Remove environments folder from version control

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  MumbaiGateway: 'https://cosmopolitan-dimensional-layer.matic-testnet.discover.quiknode.pro/183cf778fe4c2260b3b7dbc9bb264848f4f3b0ee/',
-  Contract: '0xc15a65D129D2a3D4C4Ea10270755F15E6FcF5b42'
-};
