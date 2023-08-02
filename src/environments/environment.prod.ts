e5149a2cb9558de61636749e9fed68c57bc6b3cc
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2023-08-02 21:01:41 +0530

    env check

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  MumbaiGateway: 'https://cosmopolitan-dimensional-layer.matic-testnet.discover.quiknode.pro/183cf778fe4c2260b3b7dbc9bb264848f4f3b0ee/',
+  Contract: '0xc15a65D129D2a3D4C4Ea10270755F15E6FcF5b42'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  MumbaiGateway: 'https://cosmopolitan-dimensional-layer.matic-testnet.discover.quiknode.pro/183cf778fe4c2260b3b7dbc9bb264848f4f3b0ee/',
+  Contract: '0xc15a65D129D2a3D4C4Ea10270755F15E6FcF5b42'
+
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
